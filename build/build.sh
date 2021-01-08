4d3078e91453664237bc941ff77a80609a4f35bc
Andrew Wormald <andreww@luno.com>
2021-01-08 15:40:34 +0200

    update builds

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,12 @@
 # Build script for MacOS, Linux, and Windows
 cd ../src
 
-GOOS=darwin go build -o ../bin/gowuick_amd64_darwin main.go
-GOOS=linux go build -o ../bin/gowuick_amd64_linux main.go
-GOOS=windows go build -o ../bin/gowuick_amd64_windows main.go
+GOOS=darwin go build -o ../bin/goquick_amd64_darwin main.go
+GOOS=linux go build -o ../bin/goquick_amd64_linux main.go
+GOOS=windows go build -o ../bin/goquick_amd64_windows main.go
 
 cd ../bin
 
-sudo chmod 777 gowuick_amd64_darwin
-sudo chmod 777 gowuick_amd64_linux
-sudo chmod 777 gowuick_amd64_windows
+sudo chmod 777 goquick_amd64_darwin
+sudo chmod 777 goquick_amd64_linux
+sudo chmod 777 goquick_amd64_windows
